26bcf9aafb70505666cb6bf4c8178b3b3408ee3e
walk-to-work <fallere15423@gmail.com>
2019-10-24 16:23:37 +0900

    構文木実装

--- 9cc.c
+++ 9cc.c
@@ -21,14 +21,88 @@ void PRINT( char *fmt , ... ){}
 
 #endif
 
-
 /*** トークナイザ ***/
 typedef enum{
-	TK_RESERVED,	//記号
-	TK_NUM,			//整数トークン
-	TK_EOF,			//入力終わりを表すトークン
-	TK_ERR, 		//エラー処理用
+	TK_RESERVED,  //記号
+	TK_NUM,       //整数トークン
+	TK_EOF,       //入力終わりを表すトークン
+	TK_ERR,       //エラー処理用
 } TokenKind;
+
+typedef enum{
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node{
+	NodeKind kind; // ノードの型
+	Node *lhs;     // 左辺
+	Node *rhs;     // 右辺
+	int val;       // kindがND_NUMのときのみ使う
+};
+
+
+Node *new_node( NodeKind kind , Node *lhs , Node *rhs );
+Node *new_node_num(int val);
+Node *expr();
+Node *mul();
+Node *primary();
+bool consume(char op);
+void expect( char op );
+int expect_number();
+
+Node *new_node( NodeKind kind , Node *lhs , Node *rhs ){
+	Node *node = calloc( 1 , sizeof(Node));
+	node->kind = kind;
+	node->lhs  = lhs;
+	node->rhs  = rhs;
+	return node;
+}
+
+Node *new_node_num(int val){
+	Node *node = calloc( 1 , sizeof( Node ));
+	node->kind = ND_NUM;
+	node->val  = val;
+	return node;
+}
+
+Node *expr(){
+	Node *node = mul();
+	for (;;){
+		if( consume('+'))
+			node = new_node( ND_ADD , node , mul() );
+		else if (consume('-'))
+			node = new_node( ND_SUB , node , mul() );
+		else
+			return node;
+	}
+}
+
+Node *mul(){
+	Node *node = primary();
+	for(;;){
+		if ( consume('*') )
+			node = new_node( ND_MUL , node , primary() );
+		else if( consume('/') )
+			node = new_node( ND_DIV , node , primary() );
+		else 
+			return node;
+	}
+}
+
+Node *primary(){
+	if( consume('(') ){
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+	return new_node_num(expect_number());
+}
 
 typedef struct Token Token;
 
